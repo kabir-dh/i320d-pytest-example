5db5bff38131ab9595096b17d6af1c29e5671731
Kabir Dhawan <kabir.a.dhawan@gmail.com>
2025-03-26 16:58:51 -0500

    test now documents why the tests are failing

--- my_code.py
+++ my_code.py
@@ -7,16 +7,24 @@ def fix_phone_num(phone_num_to_fix):
   four_part = phone_num_to_fix[6:] # # 8823 (last four digits)
   
   fixed_num = "(" + area_code + ")" + " " + three_part + " " + four_part 
-  
+
   return fixed_num
 
 def test_fix_phone_num():
-  assert fix_phone_num("5125558823") == '(512) 555 8823'
-  assert fix_phone_num("5554429876") == '(555) 442 9876'
-  assert fix_phone_num("3216543333") == '(321) 654 3333'
+    assert fix_phone_num("5125558823") == '(512) 555 8823'
+    assert fix_phone_num("5554429876") == '(555) 442 9876'
+    assert fix_phone_num("3216543333") == '(321) 654 3333'
 
 def test_diff_input(phone_num_to_fix):
-  assert fix_phone_num("55544298761") == '(555) 442 98761'
-  assert fix_phone_num("3216543333") == '(321) 654 3333'
-
-
+    assert fix_phone_num("555-442-98761") == '(555) 442 98761'
+    assert fix_phone_num("(321) 654 3333") == '(321) 654 3333'
+  
+def test_raise_val_error(phone_num_to_fix):
+    with pytest.raises(ValueError):
+        fix_phone_num("51")
+        
+def test_not_all_digits(phone_num_to_fix):
+    with pytest.raises(ValueError):
+        fix_phone_num("abc")
+  
+  
